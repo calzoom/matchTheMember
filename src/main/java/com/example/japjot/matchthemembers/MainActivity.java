cb8e5135f57a081c5d277b658878944049ef8969
Japjot Singh <japjot@calvisitor-10-105-134-111.calvisitor.1918.berkeley.edu>
2018-02-09 22:55:09 -0800

    added toast

--- src/main/java/com/example/japjot/matchthemembers/MainActivity.java
+++ src/main/java/com/example/japjot/matchthemembers/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.japjot.matchthemembers;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,14 +19,17 @@ import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Timer t;
+//    private static final long startTime = 5000;
+//
+//    private TextView mTextViewCountdown;
+//
+//    private CountDownTimer mCountDownTimer;
+
 
     int score = 0;
 
     ImageView imageView;
-
-    Button button;
-
+//    Button button;
     Button button1;
     Button button2;
     Button button3;
@@ -34,17 +41,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     String[] members = {"Daniel Andrews", "Nikhar Arora", "Tiger Chen", "Xin Yi Chen", "Julie Deng", "Radhika Dhomse", "Kaden Dippe", "Angela Dong", "Zach Govani", "Shubham Gupta", "Suyash Gupta", "Joey Hejna", "Cody Hsieh", "Stephen Jayakar", "Aneesh Jindal", "Mohit Katyal", "Mudabbir Khan", "Akkshay Khoslaa", "Justin Kim", "Eric Kong", "Abhinav Koppu", "Srujay Korlakunta", "Ayush Kumar", "Shiv Kushwah", "Leon Kwak", "Sahil Lamba", "Young Lin", "William Lu", "Louie McConnell", "Max Miranda", "Will Oakley", "Noah Pepper", "Samanvi Rai", "Krishnan Rajiyah", "Vidya Ravikumar", "Shreya Reddy", "Amy Shen", "Wilbur Shi", "Sumukh Shivakumar", "Fang Shuo", "Japjot Singh", "Victor Sun", "Sarah Tang", "Kanyes Thaker", "Aayush Tyagi", "Levi Walsh", "Carol Wang", "Sharie Wang", "Ethan Wong", "Natasha Wong", "Aditya Yadav", "Candice Ye", "Vineeth Yeevani", "Jeffrey Zhang"};
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         imageView = (ImageView) findViewById(R.id.imageView);
 
         r = new Random();
-
-        button = (Button) findViewById(R.id.getbutton);
-
+//        button = (Button) findViewById(R.id.getbutton);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
@@ -54,26 +62,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button2.setOnClickListener(this);
         button3.setOnClickListener(this);
         button4.setOnClickListener(this);
+        imageView.setOnClickListener(this);
 
         setUp();
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                int idxRandomPerson = r.nextInt(members.length);
-                String theMember = members[idxRandomPerson];
-
-                // new member and then display that member's pic
-//                Member test = new Member("Noah Pepper", getApplicationContext());
-
-                Member member = new Member(theMember, getApplicationContext());
-                imageView.setImageResource(member.getImage());
-
-            }
-        });
-
-
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                int idxRandomPerson = r.nextInt(members.length);
+//                String theMember = members[idxRandomPerson];
+//
+//                // new member and then display that member's pic
+////                Member test = new Member("Noah Pepper", getApplicationContext());
+//
+//                Member member = new Member(theMember, getApplicationContext());
+//                imageView.setImageResource(member.getImage());
+//
+//            }
+//        });
+//        mTextViewCountdown = findViewById(R.id.time);
 
     }
 
@@ -109,7 +116,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imageView.setImageResource(correctMemberImagez.getImage());
 
         ((TextView) findViewById(R.id.textViewScoreNumber)).setText(String.valueOf(score));
+//        resetTimer();
+//
+//        startTimer();
     }
+
+//    private void startTimer(){
+//        mCountDownTimer = new CountDownTimer(startTime, 1000) {
+//            @Override
+//            public void onTick(long millisUntilFinished) {
+//                updateCounDownText(millisUntilFinished);
+//            }
+//
+//            @Override
+//            public void onFinish() {
+//
+//            }
+//        }.start();
+//    }
+//
+//    private void updateCounDownText(long m){
+//        mTextViewCountdown.setText(String.valueOf(m/1000));
+//    }
+//
+//    private void resetTimer(){
+//        updateCounDownText(startTime);
+//    }
 
 
     @Override
@@ -118,8 +150,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             if(((Button) v).getText().equals(correctMember)){
                 score++;
             }
+            else {
+                Toast.makeText(getApplicationContext(), "Oops! That was actually: " + correctMember,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        else if(v.getId() == R.id.imageView){
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, 1);
         }
 
         setUp();
     }
 }
+
+
